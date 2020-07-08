ce0e89fdaf15d1b59712f8785dba3f8ff71c5a16
Nick Hemenway <nhemenway@wisc.edu>
2020-07-07 23:21:53 -0500

    update example

--- Examples/mass_spring_damper.py
+++ Examples/mass_spring_damper.py
@@ -10,6 +10,7 @@ import simEngine3D as sim
 #%% ############   CREATE SYSTEM   ############
 
 sys1 = sim.System()
+sys1.set_gravity([0,0,0])
 
 #%% ############   CREATE SPHERICAL MASS   ############
 
@@ -38,7 +39,7 @@ resting_pos = l0 + m*9.80665/k #position of mass at rest accounting for gravity
 h0 = 0.1
 v0 = 0
 
-bob.set_position([0, 0, -resting_pos + h0]) #bob starts sitting at springs unstretched position
+bob.set_position([0, 0, h0]) #-resting_pos + h0]) #bob starts sitting at springs unstretched position
 bob.set_vel([0, 0, v0])
 
 sys1.force_TSDA(sys1.global_frame, [0,0,0], bob, [0,0,0], k, l0, c, actuator)
@@ -74,7 +75,25 @@ while sys1.t < t_stop:
         
 #%% ############   ANALYTICAL SOLUTION   ############     
         
-# t_analytic = np.linspace(0, t_stop, 100)
+def MSD(m,k,c,x0,v0,t_arr):
+    
+    wn = np.sqrt(k/m)
+    zeta = c/(2*m*wn)
+    
+    #underdamped
+    if zeta < 1:
+        
+        wd = wn*np.sqrt(1-zeta)
+        
+        sin_terms = ((v0 + zeta*wn*x0)/wd)*np.sin(wd*t_arr) + x0*np.cos(wd*t_arr)
+        
+        y = np.exp(-zeta*wn*t_arr)*sin_terms
+        
+        return y    
+    
+t_analytic = np.linspace(0, t_stop, 100)
+
+z_analytic = MSD(m,k,c,h0,v0,t_analytic)
         
 # omega_n = np.sqrt(k/m)
 # zeta = c/(2*m*omega_n)
@@ -89,7 +108,7 @@ while sys1.t < t_stop:
 #%% ############   POST PROCESSING AND PLOTTING   ############        
         
 r = np.vstack(r)
-r += resting_pos #add resting pos so that it oscillates about zero
+# r += resting_pos #add resting pos so that it oscillates about zero
 
 v = np.vstack(v)
 v_max = v.max()
@@ -100,7 +119,7 @@ ax = fig.add_subplot(111)
 # ax.axhline(-resting_pos, ls = '--', color = 'k', zorder = -1)
 # ax.axhline(0, ls = '-', lw = 1, color = 'k', zorder = -1)
 ax.plot(time, r[:,2], label = 'Simulated')
-# ax.plot(t_analytic, z_analytic, ls = '--', label = 'Analytic')
+ax.plot(t_analytic, z_analytic, ls = '--', label = 'Analytic')
 
 ax.set_xlabel('Time, t [s]')
 ax.set_ylabel('Bob Position, $z$ [m]')
--- Examples/spinning_bar.py
+++ Examples/spinning_bar.py
@@ -81,16 +81,16 @@ while sys1.t < t_stop:
 #%% ############   ANALYTICAL SOLUTION   ############   
     
         
-# t_analytic = np.linspace(0, t_stop, 100)
+t_analytic = np.linspace(0, t_stop, 100)
 
-# omega_n = np.sqrt(1.2*k/Ixx)
-# zeta = c/(2*Ixx*omega_n)
+omega_n = np.sqrt(k/Ixx)
+zeta = c/(2*Ixx*omega_n)
 
-# #case 1: Underdamped
-# if zeta < 1:
-#     omega_d = omega_n*np.sqrt(1-zeta)
+#case 1: Underdamped
+if zeta < 1:
+    omega_d = omega_n*np.sqrt(1-zeta)
     
-#     theta_analytic = np.exp(-zeta*omega_n*t_analytic)*(theta_init*np.cos(omega_d*t_analytic) + (theta_dot_init + zeta*omega_n*theta_init)/omega_d*np.sin(omega_d*t_analytic))
+    theta_analytic = np.exp(-zeta*omega_n*t_analytic)*(theta_init*np.cos(omega_d*t_analytic) + (theta_dot_init + zeta*omega_n*theta_init)/omega_d*np.sin(omega_d*t_analytic))
         
 #%% ############   POST PROCESSING AND PLOTTING   ############        
         
@@ -102,9 +102,9 @@ ax = fig.add_subplot(111)
 
 # ax.plot(time, omega[:,0], label = 'Simulated')
 ax.plot(time, theta, label = 'Simulated')
-# ax.plot(t_analytic, theta_analytic, ls = '--', label = 'Analytic')
+ax.plot(t_analytic, theta_analytic, ls = '--', label = 'Analytic')
 
 ax.set_xlabel('Time, t [s]')
 ax.set_ylabel('Angular Displacement, $\\theta$ [$\degree$]')
-# ax.legend()
+ax.legend()
 fig.tight_layout()
